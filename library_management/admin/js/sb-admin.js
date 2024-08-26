0bdd5134ee82ba2a4c18c80b091b80b6e620ef2c
Alan Grigory Vinayan <alankvinayan3@gmail.com>
2024-08-26 07:56:27 +0530

    Update sb-admin.js

--- library_management/admin/js/sb-admin.js
+++ library_management/admin/js/sb-admin.js
@@ -23,7 +23,9 @@
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
-    } else {
+    } 
+    else 
+    {
       $('.scroll-to-top').fadeOut();
     }
   });
